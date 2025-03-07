726c34ba248d1d07e7c947c975d6e48c5f120e1a
noppharat2499 <noparat.d@ku.th>
2025-03-07 16:08:06 +0700

    Uptate vite

--- src/section/Experience/index.jsx
+++ src/section/Experience/index.jsx
@@ -1,5 +1,6 @@
 import { faGithub, faMedium, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import picExp from "../../assets/picExp1.png";
 
 const Experience = () => {
           return(
@@ -7,8 +8,12 @@ const Experience = () => {
                     <div className="text-left text-primaryAccent font-medium">Experience</div>
                     <div className="grid grid-cols-[30%_70%]">                            
                               <div>
-                               <div>2024-2025</div>
-                               <div>image</div>
+                              <div className="text-left">
+                               <span>2024-2025</span>
+                              </div>
+                              <div>
+                               <img src={picExp} className="W-5/6 rounded-md border-2" />  
+                               </div>       
                               </div>
                               <div className="flex flex-col gap-y-4">
                                <div className="text-primaryAccent">Fashion Ecommerce</div>
@@ -19,11 +24,11 @@ const Experience = () => {
                                </div>
                                <div className="text-left text-sm">Designing and developing a sports data analysis system that scrapes data from various sports websites, processes it, and presents different profit-making possibilities in real-time for customers.</div>                 
                                <div className="text-left flex gap-4 text-sm">
-                                        <div className="text-gray-100 font-medium bg-primaryTitle px-2 py-1 rounded-md">React</div>
-                                        <div className="text-gray-100 font-medium bg-primaryTitle px-2 py-1 rounded-md">Tailwind</div>
+                                        <div className="text-primaryBase font-medium bg-primaryTitle px-2 py-1 rounded-md hover:text-gray-100">React</div>
+                                        <div className="text-primaryBase font-medium bg-primaryTitle px-2 py-1 rounded-md hover:text-gray-100">Tailwind</div>
                               </div>
                               </div>
-                    </div>
+                    </div> 
                     </div>
           )
 }
